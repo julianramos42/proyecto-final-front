8c0ade82b40e07c5d6e918d034bee030a7f87036
Julian <julianete4@gmail.com>
2023-04-05 17:56:08 -0300

    fix

--- src/components/HeaderHome/HeaderHome.jsx
+++ src/components/HeaderHome/HeaderHome.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link as Anchor, useNavigate } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -3,17 +3,6 @@ import Home from '../Home/Home'
 import About from '../About/About'
 
 export default function Index() {
-  let modalState = useSelector(store => store.modalFormReducer.state)
-  
-  const dispatch = useDispatch()
-
-  function handleSignUp(){
-    dispatch(renderModal({state: 'register'}))
-  }
-
-  function handleSignIn(){
-    dispatch(renderModal({state: 'login'}))
-  }
 
   return (
     <>
