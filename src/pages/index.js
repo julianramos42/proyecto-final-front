ecc72bf64364c67e19106a4154011bddb975df40
Franco <fcalvo33@gmail.com>
2023-04-05 18:17:51 -0300

    -

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,11 @@
 import IndexLayout from "../layouts/IndexLayout";
-import Index from "./Index/Index";
 
 import { createBrowserRouter } from "react-router-dom";
-import StoresView from "./StoresView/StoresView";
-import MyStore from "./MyStore/MyStore";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <IndexLayout />,
-    children: [{ path: "/", element: <MyStore /> }],
+    children: [{ path: "/", element: <Index /> }],
   },
 ]);
