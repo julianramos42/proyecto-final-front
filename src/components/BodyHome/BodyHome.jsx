6c1837413da4fe0cf63a05800c28f4ba836cd36c
Johan Zuluaga <johan16zulu@gmail.com>
2023-04-04 22:59:53 -0500

    finalizacion about con responsive,falta finalizar nav

--- src/components/BodyHome/BodyHome.jsx
+++ src/components/BodyHome/BodyHome.jsx
@@ -5,12 +5,14 @@ import BtnAnchor from '../BtnAnchor/BtnAnchor'
 export default function BodyHome() {
   return (
     <div className='Body_Home'>
+      <div className='infoHome'>
         <div className='cont_infoHome'>
             <h2 className='text1'>Finally, a modern</h2>
             <h2 className='text2'>Online store builder for anyone</h2>
             <h2 className='text1'>Easy. For cell phones. Open source.</h2>
         </div>
-        <BtnAnchor name='Create your online store' class='color_1'/>
+        <BtnAnchor name='See all stores' class='color_1'/>
+      </div>
     </div>
   )
 }
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Home from '../Home/Home'
+import About from '../About/About'
 
 export default function Index() {
   return (
     // <div>
     //   <h1>HOLA MUNDO</h1>
     // </div>
+    <>
     <Home/>
+    <About/>
+    </>
   )
 }
