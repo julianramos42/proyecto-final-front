43bf99ee148d0f4aebf818c8d7567d2afb8a3789
Johan Zuluaga <johan16zulu@gmail.com>
2023-04-05 14:35:37 -0500

    cambio final johan

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Home from '../Home/Home'
-import About from '../About/About'
+
 
 export default function Index() {
   return (
-    // <div>
-    //   <h1>HOLA MUNDO</h1>
-    // </div>
-    <>
-    <Home/>
-    {/* <About/> */}
-    </>
+    <div>
+      <h1>HOLA MUNDO</h1>
+    </div>
   )
 }
