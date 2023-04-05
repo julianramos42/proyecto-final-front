b2be4ec966b0e8de6415d59b2eaed93aa79d6f8a
Julian <julianete4@gmail.com>
2023-04-04 21:54:18 -0300

    register terminado, falta abrir y cerrar modal y re-render de login

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -5,21 +5,64 @@ import RegisterFieldsets from '../RegisterFieldsets/RegisterFieldsets'
 import SignBtn from '../SignBtn/SignBtn'
 import { Link as Anchor } from 'react-router-dom'
 import GoBackToHome from '../GoBackToHome/GoBackToHome'
+import { useRef } from 'react'
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function RegisterForm() {
-  return (
-    <form className='register-form'>
-        <div className='register-img'>
-            <img src={register} alt='register-img'/>
-        </div>
-        <div className='register-text'>
-            <h2>Sign Up</h2>
-            <RegisterFieldsets />
-            <SignBtn text='Sign Up' />
-            {/* ACA PONER BOTON GOOGLE */}
-            <p>Already have an account? <Anchor className='link'>Log in</Anchor></p>
-            <GoBackToHome />
-        </div>
-    </form>
-  )
+    let formData = useRef()
+
+    async function handleSignUp(e) {
+        e.preventDefault()
+
+        let formInputs = []
+
+        Object.values(formData.current).forEach(e => {
+            if (e.name) {
+                formInputs.push(e.value)
+            }
+        })
+
+        let data = {
+            name: formInputs[0],
+            last_name: formInputs[1],
+            email: formInputs[2],
+            password: formInputs[3],
+        }
+
+        let url = 'http://localhost:8080/auth/signup'
+
+        try{
+            await axios.post(url, data).then(res => toast.success(res.data.message))
+        }catch (error) {
+            if(error.code === "ERR_NETWORK"){
+                toast.error('Network Error')
+            }else{
+                if (typeof error.response.data.message === 'string') {
+                    toast.error(error.response.data.message)
+                } else {
+                    error.response.data.message.forEach(err => toast.error(err))
+                }
+            }
+        }
+    }
+
+    return (
+        <form ref={formData} onSubmit={handleSignUp} className='register-form'>
+            <div className='register-img'>
+                <img src={register} alt='register-img' />
+            </div>
+            <div className='register-text'>
+                <h2>Sign Up</h2>
+                <RegisterFieldsets />
+                <SignBtn text='Sign Up' />
+                {/* ACA PONER BOTON GOOGLE */}
+                <p>Already have an account? <Anchor className='link'>Log in</Anchor></p>
+                <GoBackToHome />
+            </div>
+            <Toaster
+                position="top-right"
+            />
+        </form>
+    )
 }
--- src/components/SignBtn/SignBtn.jsx
+++ src/components/SignBtn/SignBtn.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './SignBtn.css'
-import { Link as Anchor } from 'react-router-dom'
 
 export default function SignBtn({text}) {
   return (
-    <Anchor className='sign-btn'>{text}</Anchor>
+    <input type='submit' className='sign-btn' value={text}/>
   )
 }
