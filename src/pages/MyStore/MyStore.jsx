e6aeea5347b6b5bde7d61160eae8f9fc07b787f5
Nico <juareznico61@gmail.com>
2023-04-12 19:43:56 -0300

    products ok para revisar entre todos

--- src/pages/MyStore/MyStore.jsx
+++ src/pages/MyStore/MyStore.jsx
@@ -10,7 +10,8 @@ import axios from "axios";
 import AWS from "aws-sdk";
 import toast, { Toaster } from "react-hot-toast";
 import Modal from "../../components/ModalMyStore/ModalMyStore";
-import ModalSettings from "../../components/ModalSettings/ModalSettings";
+import ModalCreateProduct from "../../components/ModaleCreateProduct/ModaleCreateProduct"
+import CardProductMyShop from "../../components/CardProductMyShop/CardProductMyShop";
 
 export default function MyStore() {
   let modalState = useSelector((store) => store.modalFormReducer.state);
@@ -21,15 +22,16 @@ export default function MyStore() {
   const headers = { headers: { Authorization: `Bearer ${token}` } };
   const [selectedFile, setSelectedFile] = useState(null);
   const [selectedBanner, setSelectedBanner] = useState(null);
-  const [reload, setReload] = useState();
+  const [reload, setReload] = useState(false);
   const [shop, setShop] = useState({});
   const [isOpen, setIsOpen] = useState(!true);
+  const [open, setOpen] = useState(!true);
   const [isClosed, setIsClosed] = useState(false);
-  const dispatch = useDispatch();
+  const [product, setProduct] = useState([]);
 
   const s3 = new AWS.S3({
-    accessKeyId: "AKIAQTTFIUBXLQVAFUVA",
-    secretAccessKey: "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",
+    accessKeyId: "AKIAQTTFIUBXP2EXKXKF",
+    secretAccessKey: "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",
     region: "sa-east-1",
   });
 
@@ -38,7 +40,7 @@ export default function MyStore() {
   }
 
   function openSettings() {
-    setIsOpen(true);
+    setOpen(true);
   }
 
   useEffect(() => {
@@ -52,11 +54,15 @@ export default function MyStore() {
     setIsClosed(false);
   }, [isClosed]);
 
+  function closeModal2() {
+    setOpen(false);
+    setIsClosed(true);
+  }
+
   function closeModal() {
     setIsOpen(false);
     setIsClosed(true);
   }
-
   async function getMyShop() {
     const token = localStorage.getItem("token");
     const headers = { headers: { Authorization: `Bearer ${token}` } };
@@ -69,7 +75,28 @@ export default function MyStore() {
       console.log(error);
     }
   }
+  
+  
+  useEffect(() => {
+    getMyProducts(shop);
+  }, [shop, reload]);
+  
+  async function getMyProducts(shop) {
+    setReload(false)
+    const token = localStorage.getItem("token");
+    const headers = { headers: { Authorization: `Bearer ${token}` } };
 
+    try {
+      if (shop._id){
+        const url = `http://localhost:8080/shop/${shop._id}/products`;
+        const response = await axios.get(url, headers);
+      setProduct(response.data.products);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
   async function handleNewShop(e) {
     e.preventDefault();
     const url = "http://localhost:8080/shop/create";
@@ -129,6 +156,7 @@ export default function MyStore() {
     setSelectedBanner(event.target.files[0]);
   }
 
+
   return (
     <>
       <NavBarStores />
@@ -149,17 +177,35 @@ export default function MyStore() {
             <div className="infoMyStore">
               <span className="buttonsContainer">
                 <div className="buttonEditStore" onClick={openModal}>
-                  Edit Store
+                  Edit Shop
                 </div>
                 {isOpen && <Modal key={isClosed} onClose={closeModal} />}
 
                 <div className="buttonEditStore" onClick={openSettings}>
-                  Settings
+                  Add product
                 </div>
-                {/* {isOpen && <ModalSettings key={isClosed} onClose={closeModal} />} */}
+                {open && <ModalCreateProduct key={isClosed} onClose={closeModal2} />}
+                
               </span>
             </div>
             <div className="viewContentMyStore">
+              <div className="headerViewContentMyStore">
+                <div className="title-cate">
+                    <p className="title-shop">{shop.name}</p>
+                    <p className="cate-shop">{shop.category}</p>
+                </div>
+              </div>
+              <div className="cont-cards-products">
+              {product.map((product) => (
+                  <CardProductMyShop
+                    key={product._id}
+                    product={product}
+                    setReload={setReload}
+                    reload={reload}
+                  />
+                ))}
+              </div>
+              
               <div className="containerCardsMyStore"></div>
             </div>
           </>
