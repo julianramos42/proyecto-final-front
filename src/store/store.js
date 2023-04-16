30a1ff0eebf00ed54c65eb29b1483503aea8430c
Julian <julianete4@gmail.com>
2023-04-16 17:09:50 -0300

    fix

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
-import modalFormReducer from './ModalForm/reducer'
-import refAboutReducer from './RefAbout/reducer'
-import refCustomersReducer from './RefCustomers/reducer'
-import refContactReducer from './RefContact/reducer'
-import refHomeReducer from './RefHome/reducer'
-
-export const store = configureStore({
-    reducer: {
-        modalFormReducer: modalFormReducer,
-        refAboutReducer: refAboutReducer,
-        refCustomersReducer: refCustomersReducer,
-        refContactReducer: refContactReducer,
-        refHomeReducer: refHomeReducer,
-    }
-})
-=======
 import modalFormReducer from "./ModalForm/reducer";
 import refAboutReducer from "./RefAbout/reducer";
 import refCustomersReducer from "./RefCustomers/reducer";
@@ -37,4 +20,3 @@ export const store = configureStore({
     favouritesReducer: favouritesReducer,
   },
 });
->>>>>>> 4c96e51f70c63c2f4b9ef6678413a5c07ac069a9
