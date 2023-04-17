edffc03c55ebe687e35a5b76b96d1b8ee5c62dd0
Franco <fcalvo33@gmail.com>
2023-04-17 17:49:19 -0300

    Iteración visual

--- src/components/HeaderShop/HeaderShop.jsx
+++ src/components/HeaderShop/HeaderShop.jsx
@@ -1,64 +1,54 @@
-import React from 'react'
-import './HeaderShop.css'
-import bag from '../../images/bag.png'
-import { useState,useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import ShopModalActions from '../../store/ShopModal/actions'
-import ShopModal from '../../components/ShopModal/ShopModal'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
+import React from "react";
+import "./HeaderShop.css";
+import bag from "../../images/bag.png";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import ShopModalActions from "../../store/ShopModal/actions";
+import ShopModal from "../../components/ShopModal/ShopModal";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-const {renderModal} = ShopModalActions
-
+const { renderModal } = ShopModalActions;
 
 export default function HeaderShop() {
-    const dispatch = useDispatch()
-    let [shop, setShop] = useState({})
-    let shopId = useParams().shopId
+  const dispatch = useDispatch();
+  let [shop, setShop] = useState({});
+  let shopId = useParams().shopId;
 
-    let banner = {
-        'background': `rgba(0, 0, 0, 0.8) url(${shop.banner})`,
+  let banner = {
+    background: `rgba(0, 0, 0, 0.7) url(${shop.banner})`,
+  };
+
+  let shopUrl = `http://localhost:8080/shop/${shopId}`;
+  async function getShop() {
+    try {
+      await axios.get(shopUrl).then((res) => setShop(res.data.shop));
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    let shopUrl = `http://localhost:8080/shop/${shopId}`
-    async function getShop() {
-        try {
-            await axios.get(shopUrl).then(res => setShop(res.data.shop))
-        } catch (err) {
-            console.log(err)
-        }
-    }
+  useEffect(() => {
+    getShop();
+  }, [shopId]);
 
-    useEffect(() => {
-        getShop()
-    }, [shopId])
+  function openModal() {
+    dispatch(renderModal({ state: true }));
+  }
 
-    function openModal(){
-        dispatch(renderModal({state: true}))
-    }
+  let modalState = useSelector((store) => store.modalShopReducer.state);
 
-    let modalState = useSelector(store => store.modalShopReducer.state)
-
-    return (
-        <div className='topShop' style={banner}>
-            {modalState ? <ShopModal /> : <></>}
-            <nav className='navShop'>
-                <img className='logoShop' src={shop.photo} alt='logo' />
-                <div className='anchorsShop'>
-                    <a>Home</a>
-                    <a className='active'>Shop</a>
-                    <a>Blog</a>
-                </div>
-                <img onClick={openModal} className='bagShop' src={bag} alt='bag' />
-            </nav>
-            {/* <div className='searchShop'>
-                    <h2>Shop</h2>
-                    <div className='containerInput'>
-                        <img src={loupe}/>
-                        <input type='text' placeholder='Find your product'/>
-                    </div>
-                </div> */}
+  return (
+    <div className="topShop" style={banner}>
+      {modalState ? <ShopModal /> : <></>}
+      <nav className="navShop">
+        <img className="logoShop" src={shop.photo} alt="logo" />
+        <div className="anchorsShop">
+          <a className="active">{shop.name}</a>
         </div>
-    )
+        <img onClick={openModal} className="bagShop" src={bag} alt="bag" />
+      </nav>
+    </div>
+  );
 }
